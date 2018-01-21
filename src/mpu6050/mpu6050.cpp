d87c812c9e50a7d610d0c05cf3f4cd68d303b4e1
Ken <kenh5657@gmail.com>
2018-01-21 16:38:49 -0500

    Kens edits

--- src/mpu6050/mpu6050.cpp
+++ src/mpu6050/mpu6050.cpp
@@ -1,23 +1,119 @@
 #include "mpu6050.h"
 
-MPU6050::MPU6050(I2CBus *bus, uint8_t adr) : I2CDevice(bus, adr) {
+// KE: Added declaration for constants.
+
+//Register list
+#define MPU6050_BASE_ADDR	0x68
+#define REG_PWR_MGMT_1		0x6B
+#define REG_GYRO_CONFIG		0x1B
+#define REG_SMPL_RATE_DIV	0x19
+
+// Globals used in the ISR.
+MPU6050 imus[IMU_MAX] = { 0 };
+
+
+
+// KE: Added libraries
+#include <math.h>
+#include <plib.h>
+#include <stdio.h>
+
+#include "global.h"
+#include "timeutil.h"
+
+
+using namespace std;
+
+
+MPU6050::MPU6050(I2CBus *bus, uint8_t id) : I2CDevice(bus, adr) {
     this->bus = bus;
-    dev.adr = adr;
+	this->id = id;
+    dev.adr = MPU6050_BASE_ADDR + id;
     connected = false;
+
+
 }
 
 bool MPU6050::isConnected() {
     return connected;
 }
 
-bool MPU6050::setup() {
+
+
+// The original setup() shell.
+/*bool MPU6050::setup() {
     // TODO: add startup stuff
     connected = true;
+}*/
+
+
+/* 
+
+ * KE: First try at porting over code.
+ * mpu6050.h changed to include arguments in declaration.
+ * imus needs to be created.
+ * imu is missing a couple of fields: id, orientation, gyro, gs, as, accel_fs,gyro_fs,q
+ * Check to see if writeReg is right.
+ * Do we need wait CNT. 
+*/
+
+/*
+ * Personal notes: id determines which IMU we are using (1 of 2).
+*/
+
+
+bool MPU6050::setup(uint8_t gfs, uint8_t afs, quatq orientation) {
+	cout << "# * Init IMU *" << id << endl;	//printf
+	this->id = id;									// id is a missing field.		
+	uint8_t d = 1 << 7;
+
+	// reset the IMU
+	writeReg(REG_PWR_MGMT_1, &d, 1);				// d pointer address, s number of bytes.
+	
+	// Do we still need the wait CNT?
+	// Yes
+
+	d = 1;
+	writeReg(REG_PWR_MGMT_1, &d, 1);
+
+	float gfs_lut[] = { 4.3633k, 8.7266k, 17.4533k, 34.9066k };
+	float afs_lut[] = { 2k, 4k, 8k, 16k }; 
+
+	imu->gyro_fs = gfs_lut[gfs];
+	imu->accel_fs = afs_lut[afs];
+	imu->gs = imu->gyro_fs / (1 << 15);
+	imu->as = imu->accel_fs / (1 << 15);
+
+	
+	// set gyro and accel full scale range
+	d = gfs << 3;	
+	writeReg(REG_GYRO_CONFIG, &d, 1);
+
+	d = afs << 3;
+	writeReg(REG_GYRO_CONFIG, &d, 1);
+
+	d = 15; 
+	writeReg(REG_SMPL_RATE_DIV, &d, 1);
+
+
+	if (quatq_abs(imu->q) < 0.9) {
+		imu->q = (quatq) { 1.0k, 0, 0, 0 };
+
+	}
+
+	imu->orientation = orientation; 
+	imu->init = true;
+	imu->errno = I2C_NOERR;					
+
+	return imu;
+
 }
+
+
 
 bool MPU6050::writeReg(uint8_t reg, uint8_t *d, uint8_t s) {
     dev.data[0] = reg;
-    memcpy(&(dev.data[1]), d, s);
+    memcpy(&(dev.data[1]), d, s);					//
     bool ret = bus->writeData(&dev, dev.data, s+1, true);
 
     return ret;
--- src/mpu6050/mpu6050.h
+++ src/mpu6050/mpu6050.h
@@ -1,6 +1,13 @@
 #ifndef _MPU6050_H
 #define _MPU6050_H
 
+/*#include "quatq.h"
+#include "vec3.h"*/
+#include <stdint.h>
+#include <iostream>
+#include <stdbool.h>
+
+
 #ifdef LIB_COMPILE
     #include "i2c/i2cbus.h"
     #include "i2c/i2cdev.h"
@@ -11,13 +18,33 @@
 
 class MPU6050 : public I2CDevice {
 public:
+
+	// KE: I've added these. I'm assuming these eventually need to be used.
+	bool init;
+	uint8_t id;
+
+	quatq orientation;
+	quatq q;
+
+	vec3 gyro;
+	
+	float gs;
+	float as;
+	
+	float accel_fs;
+	float gyro_fs;
+
+	uint8_t adr;
+	uint8_t errno;
+
+
     MPU6050(I2CBus *bus, uint8_t adr);
 
     /*
      * Implementing virtual parent method
      */
     bool isConnected();
-    bool setup();
+    bool setup(uint8_t gfs, uint8_t afs, quatq orientation);
 
     /*
      * write a register to the MPU6050
