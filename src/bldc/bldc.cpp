ec87f10965d8f73094e837a1a7f5984ff57c2131
Ken Ho <kenh5657@gmail.com>
2018-04-06 16:48:57 -0400

    wheeeeeeeeeee

--- src/bldc/bldc.cpp
+++ src/bldc/bldc.cpp
@@ -1,3 +1,12 @@
+/**
+ * Volatile: Do we still need to prevent compiler optimizations on prop.
+ * bldc_time: what is going on here?
+ * ss_cntr: Softstart?  Could you explain the mechanism for softstart.
+ * why define SS_DIV if it's just one.
+ * compare mode...erm/
+ */
+
+
 #include "bldc.h"
 
 #include <cplib>
@@ -7,14 +16,14 @@
 #include "global.h"
 #include "timeutil.h"
 
-uint32_t bldc_time = 0;
+uint32_t bldc_time = 0;	// TODO: Fragment from before.
 
 volatile bldc::bldc(uint8_t n, uint8_t npoles, uint8_t pin1, uint8_t pin2,uint8_t pin3) {
 	poles = npoles;
     soft_start = true;
     ss_cntr = 0;
     en = true;
-    SS_DIV = 1;		// Something to be said about whether we actually need SS_DIV.
+    SS_DIV = 1;				// Something to be said about whether we actually need SS_DIV.
     this -> pin1 = pin1;
     this -> pin2 = pin2;
 	this -> pin3 = pin3;
@@ -26,26 +35,85 @@ void bldc_pwm_init(){
 	// Set the counter settings.
 	PHSA = 0;	
 	PHSB = 0;
-	FRQA = 1;	// We take the highest resolution for both counters.
+	FRQA = 1;	
 	FRQB = 1;
 
 	// Configure counter to NCO mode on both cog count.
 	CTRA = NCO_SINGLE;
 	CTRB = NCO_SINGLE;
 
-	// Direction out. Output low.
+	// Direction out. Output lo to start.
 	DIRA |= 1 << pin1 | pin2 | pin3;
 	OUTA &= ~(1 << pin1 | pin2 | pin3);
 
 	// Compare mode - not a propeller thing, but just remember that it was not implemented.
 }
-	
-void bldc_set_velocity(int32_t v) {
-	velocity = v;
+
+// This is sort of similar, excecpt that 
+void drive(hi, lo, pwm){
+		// Clear.
+		OUTA &= ~(1 << lo);
+		PHSA = -pwm_time*(PWM_PERIOD/1000);
+
+		uint32_t t = CNT;
+
+		CTRA = NCO_SINGLE | pwm;
+		OUTA |= 1 << hi;
+		waitcnt(t + PWM_PERIOD);
 }
 
-void bldc_step_angle(int32_t a) {
-	elec_angle += (a*1000) % MAX_ANGLE;
+// This is the BLDC for pitch.
+void pitchCycle () {
+	uint32_t t = CNT;
+	
+	while(signal && motorNo == 1){		//Exists because I'm not sure how to stop loop.
+			if (soft_start) {
+				ss_cntr++;
+				
+				pwm_time = (ss_cntr*PWM_PERIOD)/(PWM_FREQ/SS_DIV);
+				// Clear the out register.
+				OUTA &= ~(1 << (pin1 | pin2 | pin3);
+
+				CTRA = NCO_SINGLE | pin1;
+
+				t = CNT;	// Set time here. Now set outputs;
+				PHSA = -pwm_time*(PWM_PERIOD/1000);
+				OUTA |= 0 << pin2;
+				OUTA |= 0 << pin3;
+
+				waitcnt(t + PWM_PERIOD);
+
+				if (ss_cntr >= PWM_FREQ/SS_DIV) {
+					soft_start = false;
+				}
+			}
+			else {
+				bldc_calc_pwm;
+				switch (zone){
+					case 0:
+						drive(pin1, pin3, pin2);
+						break;
+					case 1:
+						drive(pin2, pin3, pin1);
+						break;
+					case 2:
+						drive(pin2, pin1, pin3);
+						break;
+					case 3:
+						drive(pin3, pin1, pin2);
+						break;
+					case 4:
+						drive(pin3, pin2, pin1);
+						break;
+					case 5:
+						drive(pin1, pin2, pin3);
+						break;
+					}
+			}
+		else {
+			OUTA &= ~(1 << (pin1 | pin2 | pin3);
+		}
+	}
 }
 
 // Math element, direct port from old code.
@@ -63,25 +131,15 @@ inline void bldc_calc_pwm (){
 	zone_phase= (zone & 1) ? (MAX_ANGLE/6) - zone_phase : zone_phase;
 	pwm_time = zone_phase*PWM_PERIOD/(MAX_ANGLE/6);
 }
-
-
-void setDrive(hi, lo, pwm)[
-		OUTA &= ~(1 << lo);
-		OUTA |= 1 << hi;
-		CTRA = NCO_SINGLE | pwm;
-		PHSA = -pwm_time*(PWM_PERIOD/1000);
-
-	]
-
-// This is the BLDC for pitch.
-void pitchCycle (uint8_t engine, bool signal) {
-	while(signal)
-		if (motorNo = 1) {
-			if (soft_start) {
-				ss_cntr++;
-				pwm_time = (ss_cntr*PWM_PERIOD)/(PWM_FREQ/SS_DIV);
-				OUTA &= ~(1 << pin1 | pin2 | pin3);
-
-			}
-		}
+	
+void bldc_set_velocity(int32_t v) {
+	velocity = v;
 }
+
+void bldc_step_angle(int32_t a) {
+	elec_angle += (a*1000) % MAX_ANGLE;
+}
+
+
+
+
